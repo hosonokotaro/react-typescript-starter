bad22e56434a47261a1e3f77641ab02b5d634dbe
hosonokotaro <hosono1985@gmail.com>
2020-07-22 19:33:45 +0900

    update images import

--- webpack.config.ts
+++ webpack.config.ts
@@ -30,6 +30,10 @@ const config: Configuration = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader'],
+      },
     ],
   },
   resolve: {
