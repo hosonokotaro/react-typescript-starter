040802ba66831ae96bc1929e5a74b85067e30078
hosonokotaro <hosono1985@gmail.com>
2020-07-24 01:49:46 +0900

    update test

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 
 import Message from './components/Message';
 
+const pic = './images/pic.jpg';
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Hello, App.</h1>
-
+      <img src={pic} alt=""/>
       <Message text="メッセージをprops で渡す" />
     </div>
   );
