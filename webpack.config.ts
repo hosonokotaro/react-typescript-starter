a946b7d3294250c63094428f8b6970d0c855999d
hosono kotaro <hosono1985@gmail.com>
2020-12-23 15:01:20 +0900

    fix: #30 add polyfill (#31)

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,7 +12,7 @@ interface Configuration extends WebpackConfiguration {
 
 const config: Configuration = {
   mode: 'development',
-  entry: './src/index.tsx',
+  entry: ['@babel/polyfill', './src/index.tsx'],
   output: {
     path: path.resolve(__dirname, 'dist/'),
     filename: 'bundle.js',
